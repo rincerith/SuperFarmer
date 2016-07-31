9b260b2aaff2ef3dbd4d519d9bde12da9f6be97c
Rincerith <pio.sto92@gmail.com>
2016-07-31 12:32:24 +0200

    Start

---
+++ src/superfarmer/thescypion/SuperFarmer.java
@@ -0,0 +1,10 @@
+package superfarmer.thescypion;
+
+public class SuperFarmer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+
+}
